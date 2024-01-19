8c618274b27c9d2f4f92e17a4216c580454301e5
Giannidluca93 <gdluca93@gmail.com>
2024-01-19 17:40:38 -0300

    import gallery component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Gallery from '@/components/gallery';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      main page
+      <Gallery />
     </main>
   );
 }
