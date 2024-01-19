629ad2dbcfdf270e688aa8423322a3fdcc66d73b
Giannidluca93 <gdluca93@gmail.com>
2024-01-19 17:39:12 -0300

    update styles

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,8 +1,8 @@
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 const Header = () => {
   return (
-    <header className="h-10 flex justify-between items-center p-3">
+    <header className="h-20 flex justify-between items-center p-3 mb-3">
       <h2>Logo</h2>
       <RxHamburgerMenu size={30} />
     </header>
