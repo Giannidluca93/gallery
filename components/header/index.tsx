414bc061da5fcb05b1bc6801d21bbdb04ae77855
Giannidluca93 <gdluca93@gmail.com>
2024-01-22 17:24:04 -0300

    add link to logo

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,12 @@
-import { RxHamburgerMenu } from 'react-icons/rx';
+import Link from "next/link";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Header = () => {
   return (
     <header className="h-20 flex justify-between items-center p-3 mb-3">
-      <h2>Logo</h2>
+      <Link href="/">
+        <h2>Logo</h2>
+      </Link>
       <RxHamburgerMenu size={30} />
     </header>
   );
