f176ccecdbc8e7623613498d167ece65ecc0ca64
Giannidluca93 <gdluca93@gmail.com>
2024-01-22 17:18:27 -0300

    add dumydata and loop

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,15 +1,69 @@
-import GalleryImage from '../gallery-image';
+import GalleryImage from "../gallery-image";
+
+const dummyData = [
+  {
+    imageSrc:
+      "https://interactive-examples.mdn.mozilla.net/media/cc0-images/elephant-660-480.jpg",
+    height: 300,
+    width: 300,
+    caption: "test",
+    alt: "test",
+  },
+  {
+    imageSrc:
+      "https://interactive-examples.mdn.mozilla.net/media/cc0-images/elephant-660-480.jpg",
+    height: 300,
+    width: 300,
+    caption: "test",
+    alt: "test",
+  },
+  {
+    imageSrc:
+      "https://interactive-examples.mdn.mozilla.net/media/cc0-images/elephant-660-480.jpg",
+    height: 300,
+    width: 300,
+    caption: "test",
+    alt: "test",
+  },
+  {
+    imageSrc:
+      "https://interactive-examples.mdn.mozilla.net/media/cc0-images/elephant-660-480.jpg",
+    height: 300,
+    width: 300,
+    caption: "test",
+    alt: "test",
+  },
+  {
+    imageSrc:
+      "https://interactive-examples.mdn.mozilla.net/media/cc0-images/elephant-660-480.jpg",
+    height: 300,
+    width: 300,
+    caption: "test",
+    alt: "test",
+  },
+  {
+    imageSrc:
+      "https://interactive-examples.mdn.mozilla.net/media/cc0-images/elephant-660-480.jpg",
+    height: 300,
+    width: 300,
+    caption: "test",
+    alt: "test",
+  },
+];
 
 const Gallery = () => {
   return (
-    <div>
-      <GalleryImage
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/elephant-660-480.jpg"
-        height={300}
-        width={300}
-        caption="animals"
-        alt="placeholder"
-      />
+    <div className="grid grid-cols-1 gap-1">
+      {dummyData.map(({ imageSrc, height, width, caption, alt }) => (
+        <GalleryImage
+          key={`${caption + Math.random()}`}
+          imageSrc={imageSrc}
+          height={height}
+          width={width}
+          caption={caption}
+          alt={alt}
+        />
+      ))}
     </div>
   );
 };
