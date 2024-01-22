ad5b48ede5c93548317b5125df96a46b8d5bc353
Giannidluca93 <gdluca93@gmail.com>
2024-01-22 17:19:05 -0300

    wrap in link and update props

--- components/gallery-image/index.tsx
+++ components/gallery-image/index.tsx
@@ -1,22 +1,32 @@
-import Image from 'next/image';
-import { FC } from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
 
 interface Props {
-  src: string;
+  imageSrc: string;
   height: number;
   width: number;
   alt: string;
   caption: string;
 }
 
-const GalleryImage: FC<Props> = ({ src, height, width, alt, caption }) => {
+const GalleryImage: FC<Props> = ({ imageSrc, height, width, alt, caption }) => {
   return (
-    <figure className="relative">
-      <Image src={src} height={height} width={width} alt="placeholder" />
-      <figcaption className="absolute top-0 h-full w-full flex justify-center items-center text-white bg-black/50">
-        {caption}
-      </figcaption>
-    </figure>
+    <>
+      <Link href={`/collections/${caption}`}>
+        <figure className="relative cursor-pointer">
+          <Image
+            src={imageSrc}
+            height={height}
+            width={width}
+            alt="placeholder"
+          />
+          <figcaption className="absolute top-0 h-full w-full flex justify-center items-center text-white bg-black/50">
+            {caption}
+          </figcaption>
+        </figure>
+      </Link>
+    </>
   );
 };
 
