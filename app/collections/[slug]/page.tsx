b2fb41ebcb650cabf8a4f18c948a23bf731b8fa4
Giannidluca93 <gdluca93@gmail.com>
2024-01-22 17:18:02 -0300

    create dynamic page

---
+++ app/collections/[slug]/page.tsx
@@ -0,0 +1,12 @@
+const CollectionsPage = () => {
+  return (
+    <div className="min-h-screen text-center p-4">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+        ipsa cum, a illum rem nemo corrupti dolorem laboriosam
+      </p>
+    </div>
+  );
+};
+
+export default CollectionsPage;
