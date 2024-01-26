e7f8064034616bdda078f0316ab9cac976413a0f
Giannidluca93 <gdluca93@gmail.com>
2024-01-26 15:18:46 -0300

    import Navigation

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import { RxHamburgerMenu } from "react-icons/rx";
+import Link from 'next/link';
+import Navigation from '../navigation';
 
 const Header = () => {
   return (
@@ -7,18 +7,7 @@ const Header = () => {
       <Link href="/">
         <h2 className="text-2xl">Logo</h2>
       </Link>
-
-      <nav>
-        <RxHamburgerMenu className="md:hidden" size={30} />
-        <ul className="absolute w-full mt-3 z-10 bg-slate-900 text-white p-5 right-0 flex flex-col md:flex-row md:relative md:p-0 md:bg-white md:text-black">
-          <li className="p-3">
-            <Link href="/about">About Me</Link>
-          </li>
-          <li className="p-3">
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navigation />
     </header>
   );
 };
