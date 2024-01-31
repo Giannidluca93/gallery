ae5b50e44e09296cf95bbb8605ae46f02ed41562
Giannidluca93 <gdluca93@gmail.com>
2024-01-31 15:27:19 -0300

    remove contact item

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -1,15 +1,15 @@
-'use client';
+'use client'
 
-import Link from 'next/link';
-import Menu from '../menu';
-import { useState } from 'react';
+import Link from 'next/link'
+import Menu from '../menu'
+import { useState } from 'react'
 
 const Navigation = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
 
   const handleMenuClick = () => {
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(!isOpen)
+  }
 
   return (
     <nav>
@@ -17,17 +17,16 @@ const Navigation = () => {
       <ul
         className={`${
           !isOpen ? 'hidden md:flex' : 'flex'
-        } absolute w-full mt-3 z-10 bg-slate-900 text-white p-5 right-0 flex flex-col md:flex-row md:relative md:p-0 md:bg-white md:text-black`}
+        } absolute right-0 z-10 mt-3 flex w-full flex-col bg-slate-900 p-5 text-white md:relative md:flex-row md:bg-white md:p-0 md:text-black`}
       >
-        <li className="p-3">
-          <Link onClick={() => setIsOpen(false)} href="/about">About Me</Link>
-        </li>
-        <li className="p-3">
-          <Link onClick={() => setIsOpen(false)} href="/contact">Contact</Link>
+        <li className='p-3'>
+          <Link onClick={() => setIsOpen(false)} href='/about'>
+            About Me
+          </Link>
         </li>
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
