a582129f162dabd5eea8359e67e40cb0881cfc92
Giannidluca93 <gdluca93@gmail.com>
2024-02-01 17:45:46 -0300

    simple updates

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,14 +1,14 @@
-import { FC } from 'react';
-import { RxHamburgerMenu } from 'react-icons/rx';
+import { FC } from 'react'
+import { RxHamburgerMenu } from 'react-icons/rx'
 
 interface Props {
-  handleClick: () => void;
+  handleClick: () => void
 }
 
 const Menu: FC<Props> = ({ handleClick }) => {
   return (
-    <RxHamburgerMenu className="md:hidden" onClick={handleClick} size={30} />
-  );
-};
+    <RxHamburgerMenu className='md:hidden' onClick={handleClick} size={30} />
+  )
+}
 
-export default Menu;
+export default Menu
