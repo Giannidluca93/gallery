56979dc6ae936ba5aa0069db40263e4486edb8ec
Giannidluca93 <gdluca93@gmail.com>
2024-02-02 16:37:14 -0300

    change button to link

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { FaArrowDown } from 'react-icons/fa'
 import { FaArrowUp } from 'react-icons/fa'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const collections = [
   {
@@ -26,9 +27,12 @@ const Dashboard = () => {
               height={150}
               alt='placeholder'
             />
-            <button className='mt-1 w-14 bg-blue-700 p-1 text-white'>
-              edit
-            </button>
+            <Link
+              href='/dashboard/animal/edit/1'
+              className='mt-1 inline-block w-14 bg-blue-700 p-2  text-center text-white'
+            >
+              Edit
+            </Link>
           </li>
           <li>
             <Image
@@ -37,9 +41,12 @@ const Dashboard = () => {
               height={150}
               alt='placeholder'
             />
-            <button className='mt-1 w-14 bg-blue-700 p-1 text-white'>
-              edit
-            </button>
+            <Link
+              href='/dashboard/animal/edit/1'
+              className='mt-1 inline-block w-14 bg-blue-700 p-2  text-center text-white'
+            >
+              Edit
+            </Link>
           </li>
           <li>
             <Image
@@ -48,9 +55,12 @@ const Dashboard = () => {
               height={150}
               alt='placeholder'
             />
-            <button className='mt-1 w-14 bg-blue-700 p-1 text-white'>
-              edit
-            </button>
+            <Link
+              href='/dashboard/animal/edit/1'
+              className='mt-1 inline-block w-14 bg-blue-700 p-2  text-center text-white'
+            >
+              Edit
+            </Link>
           </li>
           <li>
             <Image
@@ -59,59 +69,12 @@ const Dashboard = () => {
               height={150}
               alt='placeholder'
             />
-            <button className='mt-1 w-14 bg-blue-700 p-1 text-white'>
-              edit
-            </button>
-          </li>
-        </ul>
-        <button className='align-center mb-2 flex w-2/4 items-center justify-between rounded bg-slate-500 p-2 text-white'>
-          Profiles
-          <FaArrowUp />
-        </button>
-        <ul className='mb-2 flex space-x-1'>
-          <li>
-            <Image
-              src='https:/fakeimg.pl/300x300'
-              width={150}
-              height={150}
-              alt='placeholder'
-            />
-            <button className='mt-1 w-14 bg-blue-700 p-1 text-white'>
-              edit
-            </button>
-          </li>
-          <li>
-            <Image
-              src='https:/fakeimg.pl/300x300'
-              width={150}
-              height={150}
-              alt='placeholder'
-            />
-            <button className='mt-1 w-14 bg-blue-700 p-1 text-white'>
-              edit
-            </button>
-          </li>
-          <li>
-            <Image
-              src='https:/fakeimg.pl/300x300'
-              width={150}
-              height={150}
-              alt='placeholder'
-            />
-            <button className='mt-1 w-14 bg-blue-700 p-1 text-white'>
-              edit
-            </button>
-          </li>
-          <li>
-            <Image
-              src='https:/fakeimg.pl/300x300'
-              width={150}
-              height={150}
-              alt='placeholder'
-            />
-            <button className='mt-1 w-14 bg-blue-700 p-1 text-white'>
-              edit
-            </button>
+            <Link
+              href='/dashboard/animal/edit/1'
+              className='mt-1 inline-block w-14 bg-blue-700 p-2  text-center text-white'
+            >
+              Edit
+            </Link>
           </li>
         </ul>
       </div>
